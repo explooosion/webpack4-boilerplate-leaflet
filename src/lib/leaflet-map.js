6d665245b8b1b5f918c41988afdb1f48936558cf
Robby <ta7382@gmail.com>
2018-12-08 02:04:06 +0800

    Refactor libs and web header

--- src/lib/leaflet-map.js
+++ src/lib/leaflet-map.js
@@ -2,9 +2,9 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
-import marker from '../assets/marker-icon.png';
-import marker2x from '../assets/marker-icon-2x.png';
-import markerShadow from '../assets/marker-shadow.png';
+import marker from 'leaflet/dist/images/marker-icon.png';
+import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import L from 'leaflet';
 import 'leaflet.markercluster';
